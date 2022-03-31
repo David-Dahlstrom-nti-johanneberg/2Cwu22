05ece82ce191b70fbcf4f8890d02fdc732791eca
David-Dahlstrom-nti-johanneberg <94974938+David-Dahlstrom-nti-johanneberg@users.noreply.github.com>
2022-03-31 16:39:38 +0200

    små fix och påbörjan på javascript

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+element.addEventListener("scroll", scroll);
+
+function scroll() {
+    if (document.body.scrollTop) > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("header").style.Top = "0";
+} else {
+    document.getElementById("header").style.Top = "-60";
+}
+}
