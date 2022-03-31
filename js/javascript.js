b42594a766980e90dbd6419e9e319deb91af76d2
David-Dahlstrom-nti-johanneberg <94974938+David-Dahlstrom-nti-johanneberg@users.noreply.github.com>
2022-03-31 19:48:24 +0200

    felsökning

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,9 @@ window.addEventListener('scroll', scroll);
 function scroll() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementsByTagName("header").style.top = "0px";
+        window.alert("hej")
     } else {
         document.getElementsByTagName("header").style.top = "-60px";
+        window.alert("HEJ")
     }
 }
