782499cacae8a95c4a554562181399b154312567
David-Dahlstrom-nti-johanneberg <94974938+David-Dahlstrom-nti-johanneberg@users.noreply.github.com>
2022-04-01 10:35:05 +0200

    javascript do be working

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,10 @@ window.addEventListener('scroll', scroll);
 
 function scroll() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementsByTagName("header").style.top = "0px";
+        document.querySelector("header").style.top = "0px";
+        document.querySelector("footer").style.bottom = "0px";
     } else {
-        document.getElementsByTagName("header").style.top = "-60px";
+        document.querySelector("header").style.top = "-60vh";
+        document.querySelector("footer").style.bottom = "-3vh";
     }
 }
