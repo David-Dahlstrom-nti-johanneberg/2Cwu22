07f7f06542a45ba790b5ded2ccf627e613ba6d2d
David-Dahlstrom-nti-johanneberg <94974938+David-Dahlstrom-nti-johanneberg@users.noreply.github.com>
2022-04-02 14:34:58 +0200

    Revert "felsökning"
    This reverts commit b42594a766980e90dbd6419e9e319deb91af76d2.

--- js/javascript.js
+++ js/javascript.js
@@ -3,9 +3,7 @@ window.addEventListener('scroll', scroll);
 function scroll() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementsByTagName("header").style.top = "0px";
-        window.alert("hej")
     } else {
         document.getElementsByTagName("header").style.top = "-60px";
-        window.alert("HEJ")
     }
 }
